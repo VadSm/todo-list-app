c399ca717f64983e92645a95f6cebf80d1f33f6b
VadSm <vadsmirnov2@gmail.com>
2020-02-12 12:04:14 +0300

    Add React Router, change basic structure of app

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,15 @@
 import React from 'react';
 
-import { Provider } from 'react-redux';
-import store from 'startup/redux';
-
 import Header from 'components/common/Header';
 import ToDoListContainer from 'components/todo-list/ToDoListContainer';
 
-function App() {
-  return (
-    <Provider store={store}>
-      <div className="App">
-        <div className="container">
-          <Header />
-          <ToDoListContainer />
-        </div>
-      </div>
-    </Provider>
-  );
-}
+const App = () => (
+  <div className="App">
+    <div className="container">
+      <Header />
+      <ToDoListContainer />
+    </div>
+  </div>
+);
 
 export default App;

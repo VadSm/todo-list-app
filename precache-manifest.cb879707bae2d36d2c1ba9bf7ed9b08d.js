090966f6d96561f303271263f73448afbffdf4fb
VadSm <vadsmirnov2@gmail.com>
2020-10-26 13:56:13 +0300

    Updates

---
+++ precache-manifest.cb879707bae2d36d2c1ba9bf7ed9b08d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e18509acc58cffadeac99b985e6574ca",
+    "url": "/todo-list-app/index.html"
+  },
+  {
+    "revision": "6f0a8a44b15638e5dc9b",
+    "url": "/todo-list-app/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "db76ac68b47f3ea62c2d",
+    "url": "/todo-list-app/static/css/main.9368cd8b.chunk.css"
+  },
+  {
+    "revision": "6f0a8a44b15638e5dc9b",
+    "url": "/todo-list-app/static/js/2.7e4a1243.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/todo-list-app/static/js/2.7e4a1243.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db76ac68b47f3ea62c2d",
+    "url": "/todo-list-app/static/js/main.ce4e3103.chunk.js"
+  },
+  {
+    "revision": "afa7823c543e3769ff1d",
+    "url": "/todo-list-app/static/js/runtime-main.741617dd.js"
+  }
+]);
